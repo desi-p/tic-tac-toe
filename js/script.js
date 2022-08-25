f0f07fa4cc3030a311bbc1a8946e8990e9e58bf2
Desiree P <dpricelcsw@gmail.com>
2022-08-24 21:18:14 -0500

    codewars

--- js/script.js
+++ js/script.js
@@ -1,127 +1,159 @@
 //grabs from HTML document
-let resetBtn = document.querySelector('.resetBtn');
+let resetBtn = document.querySelector('.resetBtn')
 let h2 = document.querySelector('h2')
 
-let tiles = Array.from(document.querySelectorAll('.tile'));
-
+let tiles = Array.from(document.querySelectorAll('.tile'))
 
 //object Game
 const ticTacToe = {
-  winCons : [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]],
-  player1State : [],
-  player2State : [],
-  turn : 1,
-  
-  toggleX : function(click) {
-      console.log({click})
-      console.log(this.turn)
-      if (this.turn === 1){
-          //blocks from allowing multiple selections on a tile
-          if(click.target.classList.contains('o')){
-              alert('Please choose another tile')
-          }else{
-              //allows player1 to choose tile and changes class to 'x' allowing img to display
-              click.target.classList.add('x');
-              //sets delay for showing next players turn
-            //assigns tiles to player1 selections; to be compared to winning combinations
-        this.player1State = tiles
-                        .map((t, i) =>  t.classList.contains('x') ? i : 'skip')
-                        .filter((e, i) => e !== 'skip')
-             //check if you won! 
-        if (!checkWin()){
-              setTimeout(function(){h2.innerText = `Player 2's Turn`},250)
-                //switch counter to player 2 turn
-              this.turn = 2
-        }
-      }
-      
-      } else if (this.turn === 2){
-          if(click.target.classList.contains('x')){
-            alert('Please choose another tile')
-          } else {
-            click.target.classList.add('o')
-            //assigns tiles to player1 selections; to be compared to winning combinations  
-            this.player2State = tiles
-                        .map((t, i) =>  t.classList.contains('o') ? i : 'skip')
-                        .filter((e, i) => e !== 'skip')
-        //console.log(player2State)
-    
-            //check if you won! 
-        if (!checkWin()){      
-            //sets delay for showing next players turn
-          setTimeout(function(){h2.innerText = `Player 1's Turn`},250)
-            //switch counter to player 1 turn
-          this.turn = 1
-        }
-      }
-      }
-    },
-  
-  //reset function to clear the board
-reset () {
-  this.player1State = []
-  this.player2State = []
- 
-  this.turn = 1
+  //winning combinations
+	winCons: [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	],
+  //tracking player turns and tiles
+	player1State: [],
+	player2State: [],
+	turn: 1,
 
-  tiles.forEach(tile => {
-    tile.classList.remove('x')
-    tile.classList.remove('o')
-  })
+  //toggle method - tracks turn, renders 'x' or 'o' to HTML, ensures 'x' or 'o' removed from HTML div class lists
+	toggleX: function (click) {
+  //setting variables
+		const cl = click.target.classList
+    const isEmpty = !cl.contains('x') && !cl.contains('o')
+		//console.log(this.turn)
+    //checking that classList is empty, that it's player 1's turn and no winner
+		if (isEmpty && this.turn === 1 && !this.checkWin()) {
+			//blocks from allowing multiple selections on a tile
+			if (cl.contains('o')) {
+				alert('Please choose another tile')
+			} else {
+				//allows player1 to choose tile and changes class to 'x' allowing img to display
+				click.target.classList.add('x')
+				//sets delay for showing next players turn
+				//assigns tiles to player1 selections; to be compared to winning combinations
+				this.player1State = tiles
+					.map((t, i) => (t.classList.contains('x') ? i : 'skip'))
+					.filter((e, i) => e !== 'skip')
+				//check if you won!
+				if (!this.checkWin()) {
+					setTimeout(function () {
+						h2.innerText = `Player 2's Turn`
+					}, 250)
+					//switch counter to player 2 turn
+					this.turn = 2
+				}
+			}
+      //player 2's turn
+		} else if (isEmpty && this.turn === 2 && !this.checkWin()) {
+			if (cl.contains('x')) {
+				alert('Please choose another tile')
+			} else {
+				click.target.classList.add('o')
+				//assigns tiles to player1 selections; to be compared to winning combinations
+				this.player2State = tiles
+					.map((t, i) => (t.classList.contains('o') ? i : 'skip'))
+					.filter((e, i) => e !== 'skip')
+				//console.log(player2State)
 
-  h2.innerText = `Player 1's Turn`
-  tiles.forEach(tile => tile.addEventListener('click', this.toggleX, {once : true}))
-},
+				//check if you won!
+				if (!this.checkWin()) {
+					//sets delay for showing next players turn
+					setTimeout(function () {
+						h2.innerText = `Player 1's Turn`
+					}, 250)
+					//switch counter to player 1 turn
+					this.turn = 1
+				}
+			}
+		}
+	},
 
-//check win function to compare player state array with winning combinations
-checkWin() {
-  const p1 = winCons.some(combo => {
-    return combo.every(e => this.player1State.includes(e))
-  })
-  const p2 = winCons.some(combo => {
-    return combo.every(e => this.player2State.includes(e))
-  })
+	//reset function to clear the board
+	reset() {
+		this.player1State = []
+		this.player2State = []
+    //console.log('playerState', this.player1State)
 
-  if(p1){
-    // alert('Player 1 wins!')
-    h2.innerText = `Player 1 Wins`
-    //add event listener to all board tiles 
-  tiles.forEach(tile => tile.removeEventListener('click', this.toggleX, {once : true}))
-    return true
-  } 
-  if(p2) {
-    // alert('Player 2 wins!')
-    h2.innerText = `Player 2 Wins`
-    //add event listener to all board tiles 
-  tiles.forEach(tile => tile.removeEventListener('click', this.toggleX, {once : true}))
-    return true
-  }
-  if (this.player1State.length + this.player2State.length == 9){
-    h2.innerText = "Draw!"
-    return true
-  }
-  return false
+
+		this.turn = 1
+
+		tiles.forEach((tile) => {
+			tile.classList.remove('x')
+			tile.classList.remove('o')
+		})
+
+		h2.innerText = `Player 1's Turn`
+    //console.log(tiles)
+		// tiles.forEach((tile) =>
+		// 	tile.addEventListener('click', this.toggleX)
+		// )
+	},
+
+	//check win function to compare player state array with winning combinations
+	checkWin() {
+		const p1 = this.winCons.some((combo) => {
+			return combo.every((e) => this.player1State.includes(e))
+		})
+		const p2 = this.winCons.some((combo) => {
+			return combo.every((e) => this.player2State.includes(e))
+		})
+
+		if (p1) {
+			// alert('Player 1 wins!')
+			h2.innerText = `Player 1 Wins`
+			//add event listener to all board tiles
+			tiles.forEach((tile) =>
+				tile.removeEventListener('click', this.toggleX.bind(ticTacToe))
+			)
+			return true
+		}
+		if (p2) {
+			// alert('Player 2 wins!')
+			h2.innerText = `Player 2 Wins`
+			//add event listener to all board tiles
+			tiles.forEach((tile) =>
+				tile.removeEventListener('click', this.toggleX.bind(ticTacToe))
+			)
+			return true
+		}
+		if (this.player1State.length + this.player2State.length == 9) {
+			h2.innerText = 'Draw!'
+			return true
+		}
+		return false
+	},
 }
 
-}
+//adding event listeners to each tile; at the bottom as it uses the object
+tiles.forEach((tile) =>
+	tile.addEventListener('click', ticTacToe.toggleX.bind(ticTacToe))
+)
 
-tiles.forEach(tile => tile.addEventListener('click', ticTacToe.toggleX, {once : true}));
-resetBtn.addEventListener("click", ticTacToe.reset()); 
+//add eventlistener to the reset button
+resetBtn.addEventListener('click', ticTacToe.reset.bind(ticTacToe))
 
+/*Below - previous work through, attempts and trials */
 
 //determine constructors
-  //will there be any parameters for the constructors? - no parameters...using object only. May add parameters if push goals of adding names to players is achieved 
-  //are players a constructor or property?
-  //key:value pairs
-    //tiles, winCons, player1State and player2State
+//will there be any parameters for the constructors? - no parameters...using object only. May add parameters if push goals of adding names to players is achieved
+//are players a constructor or property?
+//key:value pairs
+//tiles, winCons, player1State and player2State
 
 //methods:
-  //checkwin
-    //renders to HTML
-  //turns - player1 or player2
-    //counts turns and renders to HTML
-  //reset
-    //resets board
+//checkwin
+//renders to HTML
+//turns - player1 or player2
+//counts turns and renders to HTML
+//reset
+//resets board
 //
 
 //grabbing tiles from html doc
@@ -135,15 +167,13 @@ resetBtn.addEventListener("click", ticTacToe.reset());
 
 //let player2State = []
 
-
 //event listener for reset button:
 //let resetBtn = document.querySelector('.resetBtn').addEventListener("click",reset);
 //h2.innerText = `Player 1's Turn`
 
-
 //Turns
 //let turn = 1
-//Player1 Turn 
+//Player1 Turn
 /*let toggleX = (click) => {
   if (turn === 1){
       //blocks from allowing multiple selections on a tile
@@ -193,7 +223,7 @@ resetBtn.addEventListener("click", ticTacToe.reset());
 }
 */
 
-//add event listener to all board tiles 
+//add event listener to all board tiles
 //tiles.forEach(tile => tile.addEventListener('click', toggleX, {once : true}))
 
 /*
@@ -245,10 +275,3 @@ function checkWin() {
   return false
 }
 */
-
-
-
-
-
-
-
